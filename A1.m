3c7bbd83e01aa589124c42258ecea6a8f940480a
Alec <ajpershick@gmail.com>
2018-09-17 22:26:19 -0700

    finish ass1

--- A1.m
+++ A1.m
@@ -1,62 +1,54 @@
-image = imread('coolcat.jpg'); 
+%%% CMPT 412: Assignment #1 %%%
+%%% Alec Pershick 301190478 %%%
+
+image = imread('coolcat.jpg');
+% image=zeros(6,6,3); a(:,:,1)=magic(6); a(:,:,2)=magic(6)'; a(:,:,3)=ones(6)/2; a=a/max(a(:));
 figure() 
 subplot(1,2,1);
-imshow(image);
-shrink(image);
-% zoom(image);
+imshow(image); % Original image
+% shrink(image);
+zoom(image);
 % subsample(image, 4);
 % dim(image, 2);
 % contrast_compress(image);
 % reflect(image);
 % myrotate(image);
 
+%Reduces the size of an image by taking only pixels every "factor" pixels apart
 function subsample(image, factor)
-%     %Reduces the size of an image by taking only pixels every "factor" pixels apart
     image = image(1:factor:end,1:factor:end,:);
     subplot(1,2,2);
     imshow(image);
     size(image)
 end
 
-function shrink(image)
-%    % Makes the image half as large in both height and width. Each pixel should be the
-%    % average of 4 pixels. Assume input image has even number of rows/columns.
-    
-end
-
-function zoom(image)
-%    % Makes the image twice as large in both height and width. Each pixel in the input
-%     %image is replicated to become 4 pixels in the output image.
-
-end
-
+% return an image rotated by 90 degrees clockwise
 function myrotate(image)
-   % return an image rotated by 90 degrees clockwise
     permuted = permute(image, [2 1 3]);
     reflected_permuted = permuted(:,(end:-1:1),:);
     subplot(1,2,2);
     imshow(reflected_permuted);
 end
 
+% return an image that is reflected about the vertical axis
 function reflect(image)
-   % return an image that is reflected about the vertical axis
     image=image(:,(end:-1:1),:);
     subplot(1,2,2);
     imshow(image);
-    end
+end
 
+% return an image that is scaled to be darker by �fraction�
 function dim(image,fraction)
-   % return an image that is scaled to be darker by �fraction�
    imshow(image);
    dimmed = image/fraction;
    subplot(1,2,2);
    imshow(dimmed);
 end
 
+% take the square root of each colour channel and then
+% rescale so that the image uses the full [0,1] intensity range.
+% This is just an arbitrary modification--- the resulting image
 function contrast_compress(image)
-   % take the square root of each colour channel and then
-   % rescale so that the image uses the full [0,1] intensity range.
-   % This is just an arbitrary modification--- the resulting image
     firstChannel = sqrt(double(image(:,:,1)));
     secondChannel = sqrt(double(image(:,:,2)));
     thirdChannel = sqrt(double(image(:,:,3)));
@@ -66,5 +58,35 @@ function contrast_compress(image)
     subplot(1,2,2);
     imshow(rescaledImage);
 end
+
+% Makes the image half as large in both height and width. Each pixel should be the
+% average of 4 pixels. Assume input image has even number of rows/columns.
+function shrink(image)
+    oldSize = size(image);
+    newSize = oldSize(1) * 2;
+    newImage = zeros(newSize,newSize,3);
+    newImage((1:2:end),(1:2:end),:) = image;
+    newImage((2:2:end),(2:2:end),:) = image;
+    subplot(1,2,2);
+    imshow(newImage);
+    size(newImage)
+    newImage
+end
+
+% Makes the image twice as large in both height and width. Each pixel in the input
+%image is replicated to become 4 pixels in the output image.
+function zoom(image)
+    oldSize = size(image);
+    newSize = oldSize(1) * 2;
+    newImage = zeros(newSize,newSize,3);
+    newImage((1:2:end),(1:2:end),1) = image(:,:,1);
+    newImage((2:2:end),(2:2:end),2) = image(:,:,2);
+    newImage((2:2:end),(2:2:end),3) = image(:,:,3);
+    subplot(1,2,2);
+    imshow(newImage);
+    size(newImage)
+    newImage
+end
+
 
 
