124c6b1995457d93952e65667ca101f867a572f4
Alec <ajpershick@gmail.com>
2018-11-17 23:29:30 -0800

    figured out most of this assignmnent

--- A4/A4.m
+++ A4/A4.m
@@ -1,6 +1,11 @@
-digitDatasetPath = fullfile('C:/Users/smart/Documents/GitHub/412/A4', 'orl_faces\');
+digitDatasetPath = fullfile('/home/alec/Documents/412/A4', 'orl_faces');
 imds = imageDatastore(digitDatasetPath, ...
-    'IncludeSubfolders',true,'LabelSource','foldernames');
+    'IncludeSubfolders',true,'LabelSource','foldernames','ReadFcn', @contrastImage);
+
+% imds.ReadFcn = @smoothImage;
+% imds.ReadFcn = @contrastImage;
+
+close all;
 
 figure;
 perm = randperm(400,20);
@@ -9,7 +14,7 @@ for i = 1:20
     imshow(imds.Files{perm(i)});
 end
 
-labelCount = countEachLabel(imds)
+labelCount = countEachLabel(imds);
 
 img = readimage(imds,1);
 size(img)
@@ -20,17 +25,17 @@ numTrainFiles = 5;
 layers = [
     imageInputLayer([112 92 1])
     
-    convolution2dLayer(3,8,'Padding',1)
+    convolution2dLayer(2,8,'Padding',1)
     reluLayer
     
     maxPooling2dLayer(2,'Stride',2)
     
-    convolution2dLayer(3,16,'Padding',1)
+    convolution2dLayer(2,16,'Padding',1)
     reluLayer
     
     maxPooling2dLayer(2,'Stride',2)
     
-    convolution2dLayer(3,16,'Padding',1)
+    convolution2dLayer(2,32,'Padding',1)
     reluLayer
     
     fullyConnectedLayer(40)
@@ -38,11 +43,13 @@ layers = [
     classificationLayer];
 
 options = trainingOptions('sgdm', ...
-    'MaxEpochs',100, ...
+    'MaxEpochs',50, ...
     'ValidationData',imdsValidation, ...
-    'ValidationFrequency',30, ...
+    'ValidationFrequency',15, ...
     'Verbose',false, ...
+    'InitialLearnRate',0.01, ...
     'Plots','training-progress');
+    
 
 net = trainNetwork(imdsTrain,layers,options);
 
@@ -50,3 +57,18 @@ YPred = classify(net,imdsValidation);
 YValidation = imdsValidation.Labels;
 
 accuracy = sum(YPred == YValidation)/numel(YValidation)
+
+function image = smoothImage(filename)
+    img = imread(filename);
+    image = imgaussfilt(img, 4);
+end
+
+function image = normalizeImage(filename)
+    img = mat2gray(filename);
+    image = im2double(img);
+end
+
+function image = contrastImage(filename)
+    img = imread(filename);
+    image = imadjust(img);
+end
